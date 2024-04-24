01932565db4727ba432a9fc42b5812ff6a38c68a
wooyoung <andy241822@gmail.com>
2024-04-25 05:01:40 +0900

    [Refactor] 이유 1. 리렌더링시 상단 검색바가 남아있도록 2. 스켈레톤 UI 도입

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,71 +1,22 @@
-import React, {useRef, useState} from 'react';
+import React, {useRef} from 'react';
 import './Main.css';
 import SettingBar from "./SettingBar";
-import MovieCard from "./MovieCard";
-import {useQuery} from "@tanstack/react-query";
-import {getMovieGenres, getNowPlayingMovie} from "../api/Movie";
-import Pagination from "react-js-pagination";
 import useLanguageStore from "../store/languageStore";
-import useOrderStore from "../store/orderStore";
+import MovieCardContainer from "./MovieCardContainer";
 function Main() {
     const mainRef = useRef<HTMLDivElement>(null)
     const {language, setLanguage} = useLanguageStore()
-    const {orderType,orderBy} = useOrderStore()
-    const [activePage, setActivePage] = useState<number>(1)
-
-    const { data, isLoading, isError} =
-        useQuery(
-            { queryKey: ["movie", "now",language,activePage,{orderType:orderType,orderBy:orderBy}],
-                queryFn: () => getMovieGenres(language)
-                    .then( genre =>getNowPlayingMovie(language,genre,activePage,orderType,orderBy))
-            }
-        );
-
-    if (isError) {
-        return <h2>실패</h2>;
-    }
-    if (isLoading) {
-        return <h2>로딩중</h2>;
-    }
-    const {page, results, total_pages, total_results} = data;
-    const onChangeActivePage = (pageNum:number)=>{
-        if(pageNum>total_pages){
-            alert('마지막 페이지입니다.')
-        }
-        else {
-            mainRef.current?.scrollTo({
-                top: 0,
-                behavior: 'smooth'
-            })
-            setActivePage(pageNum)
-        }
+    const handledScroll = ()=>{
+        mainRef.current?.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        })
     }
 
     return (
         <div className={'main'} ref={mainRef}>
             <SettingBar language={language} setLanguage={setLanguage}/>
-            <div className={'movieCardContainer'}>
-                {
-                    results.map((result:any)=>
-                        <MovieCard id={result.id}
-                                   title={result.title}
-                                   vote_average={result.vote_average}
-                                   poster_path={result.poster_path}
-                                   genres={result.genres}
-                                   key={result.id}
-                        />
-                    )
-                }
-            </div>
-            <Pagination
-                totalItemsCount={total_results}
-                onChange={onChangeActivePage}
-                activePage={page}
-                itemsCountPerPage={results.length}
-                linkClass={'unActivePage'}
-                activeLinkClass={'activePage'}
-                disabledClass={'disabledPage'}
-            />
+            <MovieCardContainer handledScroll={handledScroll}/>
         </div>
     );
 }
---
+++ src/components/MovieCardContainer.tsx
@@ -0,0 +1,84 @@
+import React, {useState} from 'react';
+import MovieCard from "./MovieCard";
+import './MovieCardContainer.css'
+import Pagination from "react-js-pagination";
+import useLanguageStore from "../store/languageStore";
+import useOrderStore from "../store/orderStore";
+import {useQuery} from "@tanstack/react-query";
+import {getMovieGenres, getNowPlayingMovie} from "../api/Movie";
+import SkeletonMovieCard from "./SkeletonMovieCard";
+type movieCardContainerType ={
+    handledScroll:()=>void;
+}
+function MovieCardContainer({handledScroll}:movieCardContainerType) {
+    const {language, setLanguage} = useLanguageStore()
+    const {orderType,orderBy} = useOrderStore()
+    const [activePage, setActivePage] = useState<number>(1)
+    const onChangeActivePage = (pageNum:number)=>{
+        if(pageNum>total_pages){
+            alert('마지막 페이지입니다.')
+        }
+        else {
+            handledScroll();
+            setActivePage(pageNum)
+        }
+    }
+    const { data, isLoading, isError} =
+        useQuery(
+            { queryKey: ["movie", "now",language,activePage,{orderType:orderType,orderBy:orderBy}],
+                queryFn: () => getMovieGenres(language)
+                    .then( genre =>getNowPlayingMovie(language,genre,activePage,orderType,orderBy))
+            }
+        );
+
+    if (isError) {
+        return (
+                <h2>Load Fail Retry Plz...</h2>
+        );
+    }
+    if (isLoading) {
+        return (
+            <div className={'movieCardContainer'}>
+                <SkeletonMovieCard/>
+                <SkeletonMovieCard/>
+                <SkeletonMovieCard/>
+                <SkeletonMovieCard/>
+                <SkeletonMovieCard/>
+                <SkeletonMovieCard/>
+                <SkeletonMovieCard/>
+                <SkeletonMovieCard/>
+                <SkeletonMovieCard/>
+                <SkeletonMovieCard/>
+            </div>
+        );
+    }
+    const {page, results, total_pages, total_results} = data;
+    return (
+        <>
+            <div className={'movieCardContainer'}>
+                {
+                    results.map((result: any) =>
+                        <MovieCard id={result.id}
+                                   title={result.title}
+                                   vote_average={result.vote_average}
+                                   poster_path={result.poster_path}
+                                   genres={result.genres}
+                                   key={result.id}
+                        />
+                    )
+                }
+            </div>
+            <Pagination
+                totalItemsCount={total_results}
+                onChange={onChangeActivePage}
+                activePage={page}
+                itemsCountPerPage={results.length}
+                linkClass={'unActivePage'}
+                activeLinkClass={'activePage'}
+                disabledClass={'disabledPage'}
+            />
+        </>
+    );
+}
+
+export default MovieCardContainer;
